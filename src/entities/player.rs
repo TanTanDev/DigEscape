226912345c93f2c38f50163359ce708ca5853273
Phidelux <info@bornageek.com>
2021-07-01 18:49:09 +0200

    Make teleports instant

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -2,6 +2,7 @@ use crate::constantes;
 use crate::entities::{
     foilage::Grass,
     skeleton::{Skeleton, SkeletonBlock},
+    ai::AiState
 };
 use crate::particle_system;
 use crate::particle_system::ParticleSystemCollection;
@@ -242,7 +243,9 @@ pub fn system(
                     .get_mut(other_teleporter_index)
                     .unwrap();
                 if let Some(other_teleporter) = other_teleporter_option {
+                    // Force visual insta jump
                     player.transform.position = other_teleporter.transform.position;
+                    player.sprite.visual_position = other_teleporter.sprite.visual_position;
                     sound_collection.play(3);
                     player.sprite.blink_timer = constantes::TIME_BLINK;
                     other_teleporter.sprite.blink_timer = constantes::TIME_BLINK;
